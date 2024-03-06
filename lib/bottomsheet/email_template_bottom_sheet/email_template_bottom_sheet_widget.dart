29c5961673a10f5dac6e88d9b93b2010e7990e6a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-06 20:23:44 +0000

    Updating to latest FlutterFlow output.

--- lib/bottomsheet/email_template_bottom_sheet/email_template_bottom_sheet_widget.dart
+++ lib/bottomsheet/email_template_bottom_sheet/email_template_bottom_sheet_widget.dart
@@ -1,3 +1,4 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -108,9 +109,8 @@ class _EmailTemplateBottomSheetWidgetState
                             'subject': functions.fillSubjectPlaceHolder(
                                 widget.leadDoc!,
                                 listViewEmailTemplatesRecord.subject),
-                            'body': functions.fillEmailBodyTemplate(
-                                listViewEmailTemplatesRecord.body,
-                                widget.leadDoc!),
+                            'body':
+                                '${functions.fillEmailBodyTemplate(listViewEmailTemplatesRecord.body, widget.leadDoc!)}\\n\\nkind regards…\\n$currentUserDisplayName',
                           }
                               .entries
                               .map((MapEntry<String, String> e) =>

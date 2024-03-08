3ef48e6f0d8f783bc698b50484c84186e3a3ba72
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-08 15:43:34 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/custom_email_template_widget.dart
+++ lib/custom_code/widgets/custom_email_template_widget.dart
@@ -31,6 +31,13 @@ class CustomEmailTemplateWidget extends StatefulWidget {
 }
 
 class _CustomEmailTemplateWidgetState extends State<CustomEmailTemplateWidget> {
+  late final String initialBOdy = widget.initialEmailTemplate != null
+      ? '${widget.initialEmailTemplate!.body}'
+      : '';
+  late final String initialSubject = widget.initialEmailTemplate != null
+      ? '${widget.initialEmailTemplate!.subject}'
+      : '';
+
   final TextEditingController _subjectController = TextEditingController();
   final TextEditingController _bodyController = TextEditingController();
   final FocusNode _subjectNode = FocusNode();
@@ -38,6 +45,8 @@ class _CustomEmailTemplateWidgetState extends State<CustomEmailTemplateWidget> {
 
   @override
   void initState() {
+    _bodyController.text = initialBOdy ?? '';
+    _subjectController.text = initialSubject ?? '';
     _subjectController.addListener(() {
       FFAppState().emailSubjectHolder = _subjectController.text;
     });
@@ -55,6 +64,13 @@ class _CustomEmailTemplateWidgetState extends State<CustomEmailTemplateWidget> {
     _bodyNode.dispose();
     super.dispose();
   }
+
+  List<String> _userFields = [
+    '{u_display_name}',
+    '{u_email}',
+    '{u_job_title}',
+    '{u_phone}'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -103,49 +119,109 @@ class _CustomEmailTemplateWidgetState extends State<CustomEmailTemplateWidget> {
               ),
             ),
             const SizedBox(
-              height: 40,
+              height: 20,
               width: double.infinity,
+            ),
+            Row(
+              children: [
+                Text(
+                  'Lead Fields',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
             ),
             SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: List.generate(
-                    widget.valuePlaceHolder.length,
-                    (index) => Padding(
-                          padding: const EdgeInsets.only(right: 10),
-                          child: GestureDetector(
-                            onTap: () {
-                              if (_subjectNode.hasFocus) {
-                                _subjectController.text =
-                                    _subjectController.text +
-                                        widget.valuePlaceHolder[index] +
-                                        ' ';
-                                _subjectController.selection =
-                                    TextSelection.fromPosition(TextPosition(
-                                        offset:
-                                            _subjectController.text.length));
-                              } else if (_bodyNode.hasFocus) {
-                                _bodyController.text = _bodyController.text +
-                                    widget.valuePlaceHolder[index] +
-                                    ' ';
-                                _bodyController.selection =
-                                    TextSelection.fromPosition(TextPosition(
-                                        offset: _bodyController.text.length));
-                              }
-                            },
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 5),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  border:
-                                      Border.all(width: 1, color: Colors.grey)),
-                              child: Text(widget.valuePlaceHolder[index]),
-                            ),
-                          ),
-                        )),
+                  widget.valuePlaceHolder.length,
+                  (index) => Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: GestureDetector(
+                      onTap: () {
+                        if (_subjectNode.hasFocus) {
+                          _subjectController.text = _subjectController.text +
+                              widget.valuePlaceHolder[index] +
+                              ' ';
+                          _subjectController.selection =
+                              TextSelection.fromPosition(TextPosition(
+                                  offset: _subjectController.text.length));
+                        } else if (_bodyNode.hasFocus) {
+                          _bodyController.text = _bodyController.text +
+                              widget.valuePlaceHolder[index] +
+                              ' ';
+                          _bodyController.selection =
+                              TextSelection.fromPosition(TextPosition(
+                                  offset: _bodyController.text.length));
+                        }
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 5),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(width: 1, color: Colors.grey)),
+                        child: Text(widget.valuePlaceHolder[index]),
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            )
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  'User Fields',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                  _userFields.length,
+                  (index) => Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: GestureDetector(
+                      onTap: () {
+                        if (_subjectNode.hasFocus) {
+                          _subjectController.text = _subjectController.text +
+                              _userFields[index] +
+                              ' ';
+                          _subjectController.selection =
+                              TextSelection.fromPosition(TextPosition(
+                                  offset: _subjectController.text.length));
+                        } else if (_bodyNode.hasFocus) {
+                          _bodyController.text =
+                              _bodyController.text + _userFields[index] + ' ';
+                          _bodyController.selection =
+                              TextSelection.fromPosition(TextPosition(
+                                  offset: _bodyController.text.length));
+                        }
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 5),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(width: 1, color: Colors.grey)),
+                        child: Text(_userFields[index]),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
